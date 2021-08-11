d678b27eb5a448f7a23b1596f0b1f0e1fcde133b
ibizaSunrise <milalemekh@gmail.com>
2021-08-11 15:27:16 +0200

    first commit

---
+++ src/pages/converter.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react'
+import { fetchQuote, fetchSupportedCurrencies } from '../store/currencySlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { setAmount } from '../store/currencySlice'
+
+export default function Converter() {
+  const { supportedCurrencies, amount, quote } = useSelector(state => state.currency)
+  const dispatch = useDispatch()
+  const [value, setValue] = useState('')
+  const [message, setMessage] = useState('')
+  const [result, setResult] = useState(0)
+
+
+  useEffect(() => {
+    dispatch(fetchSupportedCurrencies())
+  }, [])
+
+  function handlerEnter(e) {
+    if (e.code === 'Enter') {
+      let str = value.trim();
+      const regExp = /^\d+\.?\d*\s+\w{3}\s+\w{2}\s+\w{3}$/;
+      if (regExp.test(str)) {
+        setMessage('')
+        let [sum, source, , cur] = str.split(' ');
+        dispatch(fetchQuote({ currencies: cur, source: source }))
+        dispatch(setAmount(sum))
+        setResult(Math.floor((amount * quote) * 100) / 100)
+
+      } else {
+        setMessage('Incorrect Data!')
+        setValue('')
+        setResult(0)
+      }
+
+    }
+  }
+  return (
+    <div>
+
+      <input
+        className="form-control form-control-lg"
+        type="text"
+        placeholder="15 usd in eur"
+        value={value} onChange={e => setValue(e.target.value)} onKeyDown={e => handlerEnter(e)}
+      ></input>
+      <input className="form-control form-control-lg mt-3" type="text" value={result}  disabled readonly></input>
+   
+      <p>{message}</p>
+
+ 
+      <table className = 'table'>
+   <thead>
+      <tr>
+         <th>Code</th>
+         <th>Name</th>
+      </tr>
+   </thead>
+   <tbody>
+   {
+     supportedCurrencies.map((el,index) => (<tr key = {index} className = {index%2 === 0 ? 'table-primary': 'table-secondary'}>
+         <td>{el[0]}</td>
+         <td>{el[1]}</td>
+       </tr>
+     ))
+   }
+   </tbody>
+</table>
+    </div>
+  )
+}
---
+++ src/store/currencySlice.js
@@ -0,0 +1,88 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+const access_key = 'a1a07be30ac9ab48c6bff8747155d4c6';
+const endpoints = ['live', 'list'];
+const baseUrl = 'http://api.currencylayer.com/'
+
+export const fetchSupportedCurrencies = createAsyncThunk(
+    'currency/fetchSupportedCurrencies',
+    async function (_, { rejectWithValue, dispatch }) {
+        try {
+            const response = await fetch(`${baseUrl}${endpoints[1]}?access_key=${access_key}`);
+
+            if (!response.ok) {
+                throw new Error('Server Error')
+            }
+
+            const data = await response.json();
+            dispatch(setSupportedCurrencies(Object.entries(data.currencies)))
+        } catch (e) {
+
+        }
+    }
+)
+
+export const fetchQuote = createAsyncThunk(
+    'currency/fetchQuote',
+    async function ({ currencies, source }, { rejectWithValue, dispatch }) {
+
+        try {
+            const response = await fetch(`${baseUrl}${endpoints[0]}?access_key=${access_key}&currencies= ${currencies} & source = ${source} `);
+            if (!response.ok) {
+                throw new Error('Server Error')
+            }
+            const data = await response.json();
+            const quote = Object.values(data.quotes);
+            dispatch(setQuote(quote[0]))
+
+            // return data;
+
+        } catch (e) {
+            return rejectWithValue(e.message)
+        }
+
+    }
+)
+//helper
+const setError = (state, action) => {
+    state.status = 'rejected';
+    state.error = action.payload
+}
+
+export const currencySlice = createSlice({
+    name: 'currency',
+    initialState: {
+        supportedCurrencies: [],
+        amount: 0,
+        quote: 0,
+
+    },
+    reducers: {
+        setAmount(state, action) {
+            state.amount = action.payload;
+        },
+        setQuote(state, action) {
+            state.quote = action.payload;
+        },
+        setSupportedCurrencies(state, action) {
+            state.supportedCurrencies = action.payload
+        }
+
+    },
+    extraReducers: {
+        [fetchQuote.pending]: (state, action) => {
+            // state.status = 'loading';
+            // state.error = null;
+        },
+        [fetchQuote.fulfilled]: (state, action) => {
+            // state.status = 'resolved';     
+        },
+        [fetchQuote.rejected]: setError,
+        [fetchSupportedCurrencies.rejected]: setError,
+
+    }
+})
+
+
+export const { setAmount, setQuote, setSupportedCurrencies } = currencySlice.actions
+export default currencySlice.reducer
